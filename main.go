52e549c227f7b2f7dbd790514dcef0639dd49715
Naoki Takezoe <takezoe@gmail.com>
2019-11-26 02:00:52 +0900

    Basic implementation

--- main.go
+++ main.go
@@ -1,23 +1,58 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
-	"time"
 	"strconv"
+	"time"
+
+	"github.com/tkuchiki/go-timezone"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: timestamp <epoc_time>")
+	var (
+		tz   = flag.String("tz", "UTC", "TimeZone (e.g. JST, UTC, PST)")
+		unit = flag.String("u", "msec", "TimeUnit (e.g. sec, msec, nano)")
+	)
+	flag.Parse()
+	args := flag.Args()
+
+	offset, _ := timezone.GetOffset(*tz)
+	loc := time.FixedZone(*tz, offset)
+
+	if len(args) < 1 {
+		flag.Usage()
 		os.Exit(1)
 	}
-	var epoc = os.Args[1]
-	if len(epoc) == 7 {
-		epoc = epoc + "000"
+
+	value := args[0]
+
+	i, e := strconv.ParseInt(value, 10, 64)
+	if e == nil {
+		// sec to msec
+		if i < 10000000000 {
+			i = i * 1000
+		}
+		// msec to nsec
+		if i < 10000000000000 {
+			i = i * 1000000
+		}
+		t := time.Unix(0, i).In(loc)
+		fmt.Println(t.Format("2006-01-02 15:04:05") + " " + *tz)
+
+	} else {
+		t, _ := time.ParseInLocation("2006-01-02 15:04:05", value, loc)
+		i := t.UnixNano()
+		if *unit == "sec" {
+			i = i / 1000000000
+		} else if *unit == "msec" {
+			i = i / 1000000
+		} else if *unit == "nano" {
+			i = i
+		} else {
+			i = i / 1000000 // default is msec
+		}
+		fmt.Println(i)
 	}
-	var i, _ = strconv.ParseInt(epoc, 10, 64)
-	fmt.Println(i)
-	var t = time.Unix(i, 0)
-	fmt.Println(t)
 }
